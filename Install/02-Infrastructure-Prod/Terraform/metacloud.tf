391b86e6e05c9641e945edb6304ecbdc38f1f54d
vallard <vallard@benincosa.com>
2016-10-28 14:52:33 -0700

    updates to adding certificates

--- Install/02-Infrastructure-Prod/Terraform/metacloud.tf
+++ Install/02-Infrastructure-Prod/Terraform/metacloud.tf
@@ -1,28 +1,78 @@
+# Metacloud kubernetes setup.  This terraform file should be able to bring up a kubernetes
+# cluster up.  We base most of this script on the extremely helpful kubernetes the hard way
+# repo from Kelsey Hightower: 
+#   https://github.com/kelseyhightower/kubernetes-the-hard-way/tree/master/docs
+# while there are many ways to set up kubernetes this seems to work well and give a deep 
+# understanding of the way it is set up.  
+
+# There are several improvements we can make in the future including using containers to 
+# install the kube components instead of the actual binaries but this works well and is
+# something that can be run on bare metal as well with perhaps some modifications. 
+
+## Node Types/Names/Quantities Variables
+
+# These are the names of the servers you want for our cluster.  This is what shows 
+# up in Openstack as the hostnames. 
+# This also includes the quantities.  3 controllers gives redundancy and is a safe pick. 
+# for the workers pick as many as you have room for.  
 variable lb_name { default = "knginx"}
 variable lb_count { default = "1" }
 variable master_name { default = "kcontroller"}
 variable master_count { default = "3" }
 variable worker_name { default = "kworker" }
 variable worker_count { default = "3" }
-variable ssh_user { default = "ubuntu" }
-variable network { default = "pipeline" }
-variable kube_image { default = "ubuntu_1604_server_cloudimg_amd64"}
-variable kube_flavor { default = "m1.large" }
-variable key_pair { default = "t5" }
-variable private_key_file { default = "~/.ssh/t5.pem"}
-variable security_group { default = "default" }
-variable ip_pool { default = "PUBLIC DO NOT MODIFY" }
-variable kube_token { default = "f00bar.f00barf00bar1234" }
 variable count_format { default = "%02d" } #server number format (01, 02, ...)
+
+## OpenStack Variables
+
+# these variables are specific to your OpenStack cluster. 
+# should be Ubuntu 16.04 and reasonable size. 
+
+variable ssh_user { default = "ubuntu" }  # what is the user name that should be used to log into the nodes? 
+variable network { default = "pipeline" } # what openstack network do we use? 
+variable kube_image { default = "ubuntu_1604_server_cloudimg_amd64"} # what image do we use? 
+variable kube_flavor { default = "m1.large" } # the flavor of the machines.  
+variable key_pair { default = "t5" } # what is the keypair name to use?  This should already have been created. 
+variable private_key_file { default = "~/.ssh/t5.pem"} # the location of your private key. 
+variable security_group { default = "default" } # openstack security group to use. 
+variable ip_pool { default = "PUBLIC DO NOT MODIFY" } # the pool of floating IP addresses. 
+
+## Kubernetes Variables
+
+# these variables are used to tweek your cluster. 
+variable kube_token { default = "f00bar.f00barf00bar1234" } # pick a token that can be used to log into the cluster. 
 variable cluster_name { default = "mykubernetes" } # name of your kubernetes cluster. 
+variable cluster_domain {default = "cluster.local" } # the internal Kube cluster domain. 
+
+# each kube worker will get assigned a /24 network from this cluster_cidr network.  so if
+# we use 10.200.0.0/16 then worker01 will get 10.200.0.0/24, worker02: 10.200.1.0/24, etc. 
+# then static host routes need to be configured for the nodes to forward traffic to the worker nodes
+# to resolve this IP range. 
+variable cluster_cidr {default = "10.200.0.0/16" }
+
+# The service cluster should not overlap with the cluster_cidr and is the network all services
+# will be constructed from.  
+variable service_cluster_net { default = "10.32.0.0/24"}
+
+# This IP is the main IP that all kube-proxies will route service requests to for services. 
+variable service_cluster_ip { default = "10.32.0.1" } # cluster IP used by the servers. 
+
+# cluster DNS has to be an IP address in the cluster network. 
+variable cluster_dns {default = "10.32.0.10"}
+
 
 provider "openstack" {
 }
+
+# we only need one floating IP address per load balancer.  The rest of the cluster sits
+# behind a firewall until we figure out how to do routing to our applications. 
 
 resource "openstack_compute_floatingip_v2" "fip" {
   pool = "${var.ip_pool}"
   count = "${var.lb_count}"
 }
+
+# the load balancers.  This is just ubuntu running nginx to reverse proxy. 
 
 resource "openstack_compute_instance_v2" "lb" {
   name = "${var.lb_name}${format(var.count_format, count.index+1) }"
@@ -40,6 +90,7 @@ resource "openstack_compute_instance_v2" "lb" {
   security_groups = ["${var.security_group}"]
 }
 
+# kube master nodes.  There should be at least three. 
 resource "openstack_compute_instance_v2" "kube-master" {
   name = "${var.master_name}${format(var.count_format, count.index+1) }"
   count = "${var.master_count}"
@@ -55,6 +106,7 @@ resource "openstack_compute_instance_v2" "kube-master" {
   security_groups = ["${var.security_group}"]
 }
 
+# kube workers.  3 or more is a good minimum number. 
 resource "openstack_compute_instance_v2" "kube-worker" {
   name = "${var.worker_name}${format(var.count_format, count.index+1) }"
   count = "${var.worker_count}"
@@ -69,6 +121,47 @@ resource "openstack_compute_instance_v2" "kube-worker" {
   key_pair = "${var.key_pair}" 
   security_groups = ["${var.security_group}"]
 }
+
+# the template file we use to create the cert files used for security. 
+data "template_file" "kubernetes-csr" {
+  template = "${file("templates/kubernetes-csr.json.tpl")}"
+  vars {
+    cluster_ip = "${var.service_cluster_ip}"
+    public_ip = "${openstack_compute_instance_v2.lb.0.network.0.floating_ip}"
+    master_nodes = "${join(",\n" , formatlist("|%s|,\n|%s|", openstack_compute_instance_v2.kube-master.*.name, openstack_compute_instance_v2.kube-master.*.network.0.fixed_ip_v4))}"
+    worker_nodes = "${join(",\n" , formatlist("|%s|,\n|%s|", openstack_compute_instance_v2.kube-worker.*.name, openstack_compute_instance_v2.kube-worker.*.network.0.fixed_ip_v4))}"
+  }
+}
+
+output "certs" {
+  value = "${data.template_file.kubernetes-csr.rendered}"
+}
+
+
+# generate the certificates
+# we assume you have cfssljson and cfssl installed.  Otherwise this will fail. 
+# see: https://github.com/kelseyhightower/kubernetes-the-hard-way/blob/master/docs/02-certificate-authority.md
+resource "null_resource" "certs12" {
+  depends_on = ["openstack_compute_instance_v2.lb","openstack_compute_instance_v2.kube-worker","openstack_compute_instance_v2.kube-master"]
+  triggers {
+    template = "${data.template_file.kubernetes-csr.rendered}"
+  }
+
+  provisioner "local-exec" {
+    command = "mkdir -p certs && cp templates/ca-csr.json templates/ca-config.json certs/"
+  }
+
+  provisioner "local-exec" {
+    command =  "echo \"${data.template_file.kubernetes-csr.rendered}\" | sed -e 's/|/\"/g' > certs/kubernetes-csr.json" 
+  }
+  provisioner "local-exec" {
+    command = "cd certs; cfssl gencert -initca ca-csr.json | cfssljson -bare ca"
+  }
+  provisioner "local-exec" {
+    command = "cd certs; cfssl gencert -ca=ca.pem -ca-key=ca-key.pem -config=ca-config.json -profile=kubernetes kubernetes-csr.json | cfssljson -bare kubernetes"
+  }
+}
+
 
 # copy the key to the master node. 
 resource "null_resource" "lb" {
@@ -194,7 +287,7 @@ data "template_file" "kube-apiserver" {
     hostname = "${element(openstack_compute_instance_v2.kube-master.*.name, count.index)}"
     hostip = "${element(openstack_compute_instance_v2.kube-master.*.network.0.fixed_ip_v4, count.index)}" 
     cluster = "${join("," , formatlist("https://%s:2379", openstack_compute_instance_v2.kube-master.*.network.0.fixed_ip_v4))}"
-    service_cluster_ip_range = "10.32.0.0/24"
+    service_cluster_ip_range = "${var.service_cluster_net}"
     service_port_range = "30000-32767"
   }
 }
@@ -206,7 +299,7 @@ data "template_file" "kube-controller-manager" {
   vars {
     hostip = "${element(openstack_compute_instance_v2.kube-master.*.network.0.fixed_ip_v4, count.index)}" 
     cluster_cidr = "10.200.0.0/16"
-    service_cluster_ip_range = "10.32.0.0/24"  # has to match the cluster ip range in above definition
+    service_cluster_ip_range = "${var.service_cluster_net}" # has to match the cluster ip range in kube-apiserver
     cluster_name = "${var.cluster_name}"
   }
 }
@@ -304,8 +397,8 @@ data "template_file" "kubelet" {
   template = "${file("templates/kubelet.service.tpl")}"
   vars {
     api_servers = "${join("," , formatlist("https://%s:6443", openstack_compute_instance_v2.kube-master.*.network.0.fixed_ip_v4))}"
-    cluster_dns = "10.32.0.10"
-    cluster_domain = "cluster.local"
+    cluster_dns = "${var.cluster_dns}"
+    cluster_domain = "${var.cluster_domain}"
   }
 }
 
